e1e836df43a7c73ae2185803dc97c284af9cb785
Renan Aquino <renan.aquino99@gmail.com>
2023-07-20 23:12:17 -0300

    feat: data numbers in hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,8 +8,29 @@ export function Hero(){
                 <div className={s.container}>
                     <div className={s.headline}>
                         <h1>Unleash your creative vision.</h1>
-                        <h3>A video editing school with hands-on projects to help you become a professional motion designer, capable of creating stunning visual effects, add great storytelling, and more.</h3>
-                        <button className={s.btn}>Learn more</button>
+                        <h3>A video editing school with hands-on projects to help you become a professional motion designer, capable of creating stunning visual effects, improve your storytelling, and more.</h3>
+                        <button className={s.btn}>Learn More</button>
+                    </div>
+
+                    <div className={s.data_numbers_box}>
+                        <div className={s.data_numbers_container}>
+                            <div className={s.data_numbers}>
+                                <p>24,000</p>
+                                <p>active students</p>
+                            </div>
+                            <div className={s.data_numbers_pipe}>|</div>
+                            <div className={s.data_numbers}>
+                                <p>92%</p>
+                                <p>employement rate</p>
+                            </div>
+                            <div className={s.data_numbers_pipe}>|</div>
+                            <div className={s.data_numbers}>
+                                <p>10</p>
+                                <p>units worldwide</p>
+                            </div>
+                     
+                        </div>
+
                     </div>
                     
                 </div>
