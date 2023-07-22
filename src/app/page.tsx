2027107a9f5d21deb5e15d11c2b3457d5f239143
Renan Aquino <renan.aquino99@gmail.com>
2023-07-21 23:10:02 -0300

    feat: added the info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import s from './page.module.css'
 import { Hero } from '@/components/Hero/Hero'
 
 export default function Home() {
   return (
    <main>
     <Hero></Hero>
+
+    <section className={s.info}>
+      <h2 className={s.title}>Headline Text Hero</h2>
+      <div className={s.container}>
+        <Image className={s.image} width='540' height='350' alt='ae pr' src='/images/dragons.jpg'/> 
+        <div className={s.info_text}>
+          <h2 className={s.title_mobile}>Headline Text Hero</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, nemo et! Iusto minus numquam placeat, ad at quas rem. Deleniti, molestias praesentium voluptate recusandae perferendis animi accusantium consequuntur placeat dignissimos.]</p>
+        </div>
+      </div>  
+    </section>
+
    </main>
   )
 }
