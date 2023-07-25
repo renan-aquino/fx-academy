6d35e8b28f892d67dc628c44640f434a0dff871b
Renan Aquino <renan.aquino99@gmail.com>
2023-07-25 18:24:47 -0300

    style: adjustments on spacing

--- src/components/Sections/Hero/Hero.tsx
+++ src/components/Sections/Hero/Hero.tsx
@@ -8,24 +8,24 @@ export function Hero(){
                 <div className={s.container}>
                     <div className={s.headline}>
                         <h1>Unleash your creative vision.</h1>
-                        <h4>A video editing school with hands-on projects to help you become a professional motion designer, capable of creating stunning visual effects, improve your storytelling, and more.</h4>
+                        <h4>A video editing school with hands-on projects. Create stunning visual effects, improve your storytelling, and become a professional motion designer.</h4>
                         <button className={s.btn}>Learn More</button>
                     </div>
 
                     <div className={s.data_numbers_box}>
                         <div className={s.data_numbers_container}>
                             <div className={s.data_numbers}>
-                                <p className={s.data_number_digits}>24,000</p>
+                                <h4 className={s.data_number_digits}>24,000</h4>
                                 <p className={s.data_numbers_description}>active students</p>
                             </div>
                             <div className={s.data_numbers_pipe}>|</div>
                             <div className={s.data_numbers}>
-                                <p>92%</p>
+                                <h4>92%</h4>
                                 <p className={s.data_numbers_description}>employement rate</p>
                             </div>
                             <div className={s.data_numbers_pipe}>|</div>
                             <div className={s.data_numbers}>
-                                <p>10</p>
+                                <h4>10</h4>
                                 <p className={s.data_numbers_description}>units worldwide</p>
                             </div>
                      
