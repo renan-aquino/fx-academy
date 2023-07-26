2fe74a854d235e59b5bc6514b07a68b573375527
Renan Aquino <renan.aquino99@gmail.com>
2023-07-26 14:31:56 -0300

    fix: CTA is now responsive

--- src/components/Sections/CTA/Cta.tsx
+++ src/components/Sections/CTA/Cta.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image'
 import s from './cta.module.css'
+import timeline from '@/../public/images/timeline.jpg'
+
 
 export function Cta(){
     return(
         <section className={s.cta}>
             <div className={s.box}>
-                <div className={s.image} style={{backgroundImage: "url('/images/timeline.jpg')"}}></div>
+                {/* <div className={s.image} style={{backgroundImage: "url('/images/timeline.jpg')"}}></div> */}
+                <Image className={s.image} src={timeline} alt='a' />
                 <div className={s.text}>
                     <h2>Join our leading video editing school!</h2>
                     <p>Master the art of video editing with our expert-led courses and launch your creative career today!</p>
