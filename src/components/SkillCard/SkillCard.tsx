adb57aa8c2e4a1707d6c18165b0f83241844fd55
Renan Aquino <renan.aquino99@gmail.com>
2023-07-27 20:28:04 -0300

    fix: fixed responsiveness

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -1,16 +1,17 @@
 
 import s from './skillCard.module.css'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 
 interface skillCardProps {
-    src: string,
+    src: StaticImageData,
     description: string
 }
 
 export function SkillCard(props: skillCardProps){
     return (
         <div className={s.card}>
-            <Image src={props.src} className={s.image} alt='b' width='280' height='200'></Image>
+             {/* <Image src={props.src} className={s.image} alt='b' width='280' height='200'></Image> */}
+             <Image src={props.src} className={s.image} alt='b'></Image>
             <div className={s.description}>{props.description}</div>
         </div>
     )
--- src/components/SkillGrid/SkillGrid.tsx
+++ src/components/SkillGrid/SkillGrid.tsx
@@ -1,15 +1,28 @@
 import s from './skillGrid.module.css'
 import { SkillCard } from "../SkillCard/SkillCard";
+import biscuits from '@/../public/images/biscuits2.jpg'
+import rigGirl from '@/../public/images/rig-girl2.jpg'
+import transition from '@/../public/images/transition2.jpg'
+import colorCorrection from '@/../public/images/color-correction2.jpg'
+import fxGirl from '@/../public/images/fx-girl2.jpg'
+import expressions from '@/../public/images/extend.png'
 
 export function SkillGrid(){
     return (
         <div className={s.grid}>
-            <SkillCard src='/images/biscuits2.jpg' description='3D Rendering'/>
+            {/* <SkillCard src='/images/biscuits2.jpg' description='3D Rendering'/>
             <SkillCard src='/images/rig-girl2.jpg' description='Character Rig'/>
             <SkillCard src='/images/transition2.jpg' description='Transitions'/>
             <SkillCard src='/images/color-correction2.jpg' description='Color Correction'/>
             <SkillCard src='/images/fx-girl2.jpg' description='Video Effects'/>
-            <SkillCard src='/images/extend.png' description='Expressions'/>
+            <SkillCard src='/images/extend.png' description='Expressions'/> */}
+
+            <SkillCard src={biscuits} description='3D Rendering'/>
+            <SkillCard src={rigGirl} description='Character Rig'/>
+            <SkillCard src={transition} description='Transitions'/>
+            <SkillCard src={colorCorrection} description='Color Correction'/>
+            <SkillCard src={fxGirl} description='Video Effects'/>
+            <SkillCard src={expressions} description='Expressions'/>
         </div>
     )
 }
