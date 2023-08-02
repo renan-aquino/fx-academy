69d9e27edd9c8464ca8d01cd340552aaf524a57f
Renan Aquino <renan.aquino99@gmail.com>
2023-08-01 21:24:15 -0300

    style: scroll animation

--- src/components/Sections/Softwares/Softwares.tsx
+++ src/components/Sections/Softwares/Softwares.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import s from './softwares.module.css'
 import Image from 'next/image'
 import creativeCloud from '@/../public/vectors/CC-logo.svg'
@@ -5,11 +7,53 @@ import AElogo from '@/../public/vectors/AE-logo.svg'
 import PPlogo from '@/../public/vectors/PP-logo.svg'
 import PSlogo from '@/../public/vectors/PS-logo.svg'
 import AIlogo from '@/../public/vectors/AI-logo.svg'
+import { useEffect, useRef } from 'react'
 
 export function Softwares(){
+
+    const refContainer = useRef(null);
+    const  delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    useEffect(() => {
+        const handleScrollAnimation = (entries: IntersectionObserverEntry[]) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting){
+                    entry.target.classList.add(s.container_animation);          
+                }
+            })          
+
+        };
+    
+        const observer = new IntersectionObserver(handleScrollAnimation);
+        observer.observe(refContainer.current);
+        return () =>  observer.disconnect();      
+
+    }, []);
+
+
+    useEffect(() => {
+        
+        const elements = document.querySelectorAll<HTMLElement>(`.${s.logo}`);
+
+        const handleScrollAnimation = (entries: IntersectionObserverEntry[]) => {
+            entries.forEach(async (entry, index) => {
+                if(entry.isIntersecting){
+                await delay(200 * index);
+                entry.target.classList.add(s.logo_animation);  
+                }
+            })
+        };
+
+        const observer = new IntersectionObserver(handleScrollAnimation);
+        elements.forEach((el) => observer.observe(el));
+        return () =>  observer.disconnect();
+        
+    }, []);
+
+
     return(
         <section className={s.softwares}>
-            <div className={s.container}>
+        <div className={s.container} ref={refContainer}>
                 <div className={s.creative_cloud}>
                     <Image className={s.cc_logo} src={creativeCloud} alt='creative cloud logo' ></Image>
                 </div>
@@ -19,7 +63,7 @@ export function Softwares(){
                         <h2>Explore the best editing softwares</h2>
                         <p>The Adobe Creative Cloud provides reknown applications used by professionals worldwide. In our course, you'll master After Effects, Premiere Pro, Photoshop, Illustrator, and more.</p>
                     </div>
-                    <div className={s.logos}>
+                    <div className={s.logos} >
                       <Image className={s.logo} src={AElogo} alt='after effects logo' ></Image>
                       <Image className={s.logo} src={PPlogo} alt='premiere pro logo' ></Image>
                       <Image className={s.logo} src={PSlogo} alt='photshop logo' ></Image>
