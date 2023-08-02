687027af7873c3bd3cd9e8238d52794ff0b9b510
Renan Aquino <renan.aquino99@gmail.com>
2023-08-02 01:31:42 -0300

    style: added title scroll animation

--- src/components/Sections/Skills/Skills.tsx
+++ src/components/Sections/Skills/Skills.tsx
@@ -1,11 +1,29 @@
+'use client'
+
 import { SkillGrid } from '@/components/SkillGrid/SkillGrid'
 import s from './skills.module.css'
+import { useEffect, useRef } from 'react'
 
 export function Skills(){
+    const h2Ref = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting){
+                    entry.target.classList.add(s.title_animation);
+                }
+            })
+        }, {threshold: .5});
+
+        observer.observe(h2Ref.current);
+
+    }, []);
+
     return(
         <section className={s.skills}>
             <div className={s.container}>
-                <h2>Everything you need, in one place</h2>
+                <h2 ref={h2Ref}>Everything you need, in one place</h2>
                 <SkillGrid/>
             </div>
         </section>
