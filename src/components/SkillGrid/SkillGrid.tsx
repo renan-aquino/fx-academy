060bdaff408fd3b2594adab8422c1709ddd5bdfa
Renan Aquino <renan.aquino99@gmail.com>
2023-08-02 00:00:43 -0300

    style: added scroll animation

--- src/components/SkillGrid/SkillGrid.tsx
+++ src/components/SkillGrid/SkillGrid.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import s from './skillGrid.module.css'
 import { SkillCard } from "../SkillCard/SkillCard";
 import biscuits from '@/../public/images/biscuits2.jpg'
@@ -6,16 +8,51 @@ import transition from '@/../public/images/transition2.jpg'
 import colorCorrection from '@/../public/images/color-correction2.jpg'
 import fxGirl from '@/../public/images/fx-girl2.jpg'
 import expressions from '@/../public/images/extend.png'
+import { useEffect } from 'react';
 
 export function SkillGrid(){
+    
+    useEffect(() => {
+        
+        const elements = document.querySelectorAll<HTMLElement>(`.${s.card}`);
+        const  delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+        const handleScrollAnimation = (entries: IntersectionObserverEntry[]) => {
+            entries.forEach(async (entry, index) => {
+                if(entry.isIntersecting){
+                await delay(100 * index);
+                entry.target.classList.add(s.card_animation);  
+                }
+            })
+        };
+    
+        const observer = new IntersectionObserver(handleScrollAnimation);
+        elements.forEach((el) => observer.observe(el));
+        return () =>  observer.disconnect();
+        
+    }, []);
+
+
     return (
         <div className={s.grid}>
-            <SkillCard src={biscuits} description='3D Rendering' alt='3d rendered biscuits'/>
-            <SkillCard src={rigGirl} description='Character Rig' alt='rig character girl'/>
-            <SkillCard src={transition} description='Transitions' alt='transition icon'/>
-            <SkillCard src={colorCorrection} description='Color Correction' alt='color correction photo'/>
-            <SkillCard src={fxGirl} description='Video Effects' alt='visual effects photo'/>
-            <SkillCard src={expressions} description='Expressions' alt='extend script expression'/>
+            <div className={s.card}>
+                <SkillCard src={biscuits} description='3D Rendering' alt='3d rendered biscuits'/>
+            </div>
+            <div className={s.card}>
+                <SkillCard src={rigGirl} description='Character Rig' alt='rig character girl'/>
+            </div>
+            <div className={s.card}>
+                <SkillCard src={transition} description='Transitions' alt='transition icon'/>
+            </div>
+            <div className={s.card}>
+                <SkillCard src={colorCorrection} description='Color Correction' alt='color correction photo'/>
+            </div>
+            <div className={s.card}>
+                <SkillCard src={fxGirl} description='Video Effects' alt='visual effects photo'/>
+            </div>
+            <div className={s.card}>
+                <SkillCard src={expressions} description='Expressions' alt='extend script expression'/>
+            </div>
         </div>
     )
 }
