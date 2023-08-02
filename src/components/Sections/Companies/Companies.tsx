82bb2b25330ab0745a2b1b82c5485eabc63da712
Renan Aquino <renan.aquino99@gmail.com>
2023-08-02 03:43:05 -0300

    style: added scroll animation

--- src/components/Sections/Companies/Companies.tsx
+++ src/components/Sections/Companies/Companies.tsx
@@ -1,20 +1,58 @@
+'use client'
+
 import s from './companies.module.css'
 import dreamWorks from '@/../public/vectors/DreamWorks2.svg';
 import weta from '@/../public/vectors/Weta3.svg';
 import industrial from '@/../public/vectors/industrial-light-magic4.svg';
 import pixar from '@/../public/vectors/Pixar5.svg';
 import Image from 'next/image';
+import { useEffect, useRef } from 'react';
 
 export function Companies(){
+    const h2Ref = useRef();
+    const  delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    useEffect(() => {
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting){
+                    entry.target.classList.add(s.fade_animation);
+                }
+            })
+        }, {threshold: .5});
+
+        observer.observe(h2Ref.current);
+        return () =>  observer.disconnect();
+    }, []);
+
+    useEffect(() => {
+        const elements = document.querySelectorAll<HTMLElement>(`.${s.logo}`);
+
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(async (entry, index) => {
+                if(entry.isIntersecting){
+                    await delay(100 * index);
+                    entry.target.classList.add(s.fade_animation);
+                }
+            })
+        }, {threshold: .5});
+
+        elements.forEach((el) => observer.observe(el));
+        return () =>  observer.disconnect();
+        
+    }, []);
+
     return(
     <section className={s.companies}>    
         <div className={s.container}>
-            <h2>Companies hiring our students</h2>
+            <h2 ref={h2Ref}>Companies hiring our students</h2>
             <div className={s.company_logos}>
                 <Image className={s.logo} src={dreamWorks} alt='dream works logo'/>
                 <Image className={s.logo} src={weta} alt='weta fx logo'/>
                 <Image className={s.logo} src={industrial} alt='industrial light and magic logo'/>
-                <Image className={s.logo} src={pixar} id={s.pixar} alt='pixar logo'/>
+                <Image className={s.logo} src={pixar} alt='pixar logo'/>
             </div>
         </div>
     </section>     
--- src/components/Sections/SocialProof/SocialProof.tsx
+++ src/components/Sections/SocialProof/SocialProof.tsx
@@ -1,22 +1,63 @@
+'use client'
+
 import Image from 'next/image'
 import s from './socialProof.module.css'
 import woman from '@/../public/images/girl-portrait.jpg'
+import { useEffect, useRef } from 'react'
 
 export function SocialProof(){
+    
+    const h2Ref = useRef();
+    const boxRef = useRef();
+
+    useEffect(() => {
+        const elements = document.querySelectorAll<HTMLElement>('#listItem');
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting){
+                    entry.target.classList.add(s.fade_animation);
+                }
+            })
+        }, {threshold: .5});
+
+        observer.observe(h2Ref.current);
+        elements.forEach((el) => observer.observe(el));
+        return () =>  observer.disconnect();
+
+    }, []);
+
+    useEffect(() => {
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting){
+                    entry.target.classList.add(s.fade_animation);
+                }
+            })
+        }, {threshold: .5});
+
+        observer.observe(boxRef.current);
+        return () =>  observer.disconnect();
+
+    }, []);
+
+   
+
     return(
         <section className={s.social_proof}>
             <div className={s.container}>
                 <div className={s.topics}>
-                    <h2>The perfect enviroment for you</h2>
+                    <h2 ref={h2Ref}>The perfect enviroment for you</h2>
                     <ul className={s.bullet_points}>
-                        <li>From beginner to advanced levels</li>
-                        <li>Direct support from instructors</li>
-                        <li>Real life job experiences</li> 
-                        <li>Lifetime access to exclusive assets</li>
-                        <li>Presential or remote access to powerful computers</li>
+                        <li id='listItem' style={{animationDelay: '.1s'}}>From beginner to advanced levels</li>
+                        <li id='listItem' style={{animationDelay: '.2s'}}>Direct support from instructors</li>
+                        <li id='listItem' style={{animationDelay: '.3s'}}>Real life job experiences</li> 
+                        <li id='listItem' style={{animationDelay: '.4s'}}>Lifetime access to exclusive assets</li>
+                        <li id='listItem' style={{animationDelay: '.5s'}}>Presential or remote access to powerful computers</li>
                     </ul>
                 </div>
-                <div className={s.box}>
+                <div className={s.box} ref={boxRef}>
                     <p className={s.testimony}>
                         "I got my first job as a video editor thanks to FX Academy. I learned techniques that I use everyday, and I'm always progressing. The instructors are really cool, which keeps me motivated. Nowadays, I work as a motion designer, and I couldn't be happier!"
                     </p>
